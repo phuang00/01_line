d52eceff9bd96aa68993976cee49609c7638a051
phuang00 <phuang00@stuy.edu>
2020-02-06 15:34:01 -0500

    got octant 2 to work

--- draw.c
+++ draw.c
@@ -7,8 +7,12 @@
 
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
+  if (x0 > x1){
+    draw_line(x1, y1, x0, y0, s, c);
+  }
   int A = y1 - y0;
   int B = -(x1 - x0);
+  int d;
   if (B == 0){
     while (y0 <= y1){
       plot(s, c, x0, y0);
@@ -18,7 +22,16 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
   else{
     double slope = (double)(A)/(-B);
     if (slope > 1){
-
+      d = A + 2* B;
+      while (y0 <= y1) {
+        plot(s, c, x0, y0);
+        if (d < 0){
+          x0++;
+          d += 2 * A;
+        }
+        y0++;
+        d += 2 * B;
+      }
     }
     else if (slope > 0){
 
@@ -27,7 +40,7 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
 
     }
     else {
-      
+
     }
   }
 }

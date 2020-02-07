005299ecb19324b09cc8141ff09796b5b6214ef2
phuang00 <phuang00@stuy.edu>
2020-02-06 21:03:26 -0500

    small changes

--- draw.c
+++ draw.c
@@ -7,26 +7,26 @@
 
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
-  if (x0 > x1){
+  if (x0 > x1) {
     draw_line(x1, y1, x0, y0, s, c);
   }
   else {
     int A = y1 - y0;
     int B = -(x1 - x0);
     int d;
-    if (B == 0){
-      while (y0 <= y1){
+    if (B == 0) {
+      while (y0 <= y1) {
         plot(s, c, x0, y0);
         y0++;
       }
     }
-    else{
-      double slope = (double)(A)/(-B);
-      if (slope > 1){
+    else {
+      double slope = (double)(A) / (-B);
+      if (slope > 1) {
         d = A + 2 * B;
         while (y0 <= y1) {
           plot(s, c, x0, y0);
-          if (d < 0){
+          if (d < 0) {
             x0++;
             d += 2 * A;
           }
@@ -34,11 +34,11 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
           d += 2 * B;
         }
       }
-      else if (slope > 0){
+      else if (slope > 0) {
         d = 2 * A + B;
-        while (x0 <= x1){
+        while (x0 <= x1) {
           plot(s, c, x0, y0);
-          if (d > 0){
+          if (d > 0) {
             y0++;
             d += 2 * B;
           }
@@ -46,12 +46,12 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
           d += 2 * A;
         }
       }
-      else if (slope > -1){
+      else if (slope > -1) {
         d = 2 * A - B;
-        while (x0 <= x1){
+        while (x0 <= x1) {
           //printf("%d %d\n", x0, y0);
           plot(s, c, x0, y0);
-          if (d > 0){
+          if (d > 0) {
             y0--;
             d += 2 * B;
           }
@@ -61,10 +61,10 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
       }
       else {
         d = A - 2 * B;
-        while (y1 <= y0){
+        while (y0 >= y1) {
           //printf("%d %d\n", x0, y0);
           plot(s, c, x0, y0);
-          if (d < 0){
+          if (d < 0) {
             x0++;
             d -= 2 * A;
           }

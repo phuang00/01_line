bf4357737711d7ea9291ecd04df7e509065426f2
phuang00 <phuang00@stuy.edu>
2020-02-06 20:58:58 -0500

    got negative slopes to work

--- draw.c
+++ draw.c
@@ -10,46 +10,68 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
   if (x0 > x1){
     draw_line(x1, y1, x0, y0, s, c);
   }
-  int A = y1 - y0;
-  int B = -(x1 - x0);
-  int d;
-  if (B == 0){
-    while (y0 <= y1){
-      plot(s, c, x0, y0);
-      y0++;
-    }
-  }
-  else{
-    double slope = (double)(A)/(-B);
-    if (slope > 1){
-      d = A + 2* B;
-      while (y0 <= y1) {
+  else {
+    int A = y1 - y0;
+    int B = -(x1 - x0);
+    int d;
+    if (B == 0){
+      while (y0 <= y1){
         plot(s, c, x0, y0);
-        if (d < 0){
-          x0++;
-          d += 2 * A;
-        }
         y0++;
-        d += 2 * B;
       }
     }
-    else if (slope > 0){
-      d = 2 * A + B;
-      while (x0 < x1){
-        plot(s, c, x0, y0);
-        if (d > 0){
+    else{
+      double slope = (double)(A)/(-B);
+      if (slope > 1){
+        d = A + 2 * B;
+        while (y0 <= y1) {
+          plot(s, c, x0, y0);
+          if (d < 0){
+            x0++;
+            d += 2 * A;
+          }
           y0++;
           d += 2 * B;
         }
-        x0++;
-        d += 2 * A;
       }
-    }
-    else if (slope > -1){
-
-    }
-    else {
-
+      else if (slope > 0){
+        d = 2 * A + B;
+        while (x0 <= x1){
+          plot(s, c, x0, y0);
+          if (d > 0){
+            y0++;
+            d += 2 * B;
+          }
+          x0++;
+          d += 2 * A;
+        }
+      }
+      else if (slope > -1){
+        d = 2 * A - B;
+        while (x0 <= x1){
+          //printf("%d %d\n", x0, y0);
+          plot(s, c, x0, y0);
+          if (d > 0){
+            y0--;
+            d += 2 * B;
+          }
+          x0++;
+          d -= 2 * A;
+        }
+      }
+      else {
+        d = A - 2 * B;
+        while (y1 <= y0){
+          //printf("%d %d\n", x0, y0);
+          plot(s, c, x0, y0);
+          if (d < 0){
+            x0++;
+            d -= 2 * A;
+          }
+          y0--;
+          d += 2 * B;
+        }
+      }
     }
   }
 }
--- main.c
+++ main.c
@@ -17,32 +17,44 @@ int main() {
 
   clear_screen(s);
   //octants 1 and 5
+  //printf("a\n");
   draw_line(0, 0, XRES-1, YRES-1, s, c);
+  //printf("b\n");
   draw_line(0, 0, XRES-1, YRES / 2, s, c);
+  //printf("c\n");
   draw_line(XRES-1, YRES-1, 0, YRES / 2, s, c);
 
   //octants 8 and 4
   c.blue = 255;
+  //printf("d\n");
   draw_line(0, YRES-1, XRES-1, 0, s, c);
+  //printf("e\n");
   draw_line(0, YRES-1, XRES-1, YRES/2, s, c);
+  //printf("f\n");
   draw_line(XRES-1, 0, 0, YRES/2, s, c);
 
   //octants 2 and 6
   c.red = 255;
   c.green = 0;
   c.blue = 0;
+  //printf("g\n");
   draw_line(0, 0, XRES/2, YRES-1, s, c);
+  //printf("h\n");
   draw_line(XRES-1, YRES-1, XRES/2, 0, s, c);
 
   //octants 7 and 3
   c.blue = 255;
+  //printf("i\n");
   draw_line(0, YRES-1, XRES/2, 0, s, c);
+  //printf("j\n");
   draw_line(XRES-1, 0, XRES/2, YRES-1, s, c);
 
   //horizontal and vertical
   c.blue = 0;
   c.green = 255;
+  //printf("k\n");
   draw_line(0, YRES/2, XRES-1, YRES/2, s, c);
+  //printf("l\n");
   draw_line(XRES/2, 0, XRES/2, YRES-1, s, c);
 
   display(s);
